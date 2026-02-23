d95e3a5fcd3f47df8d52af78dbec4113fb7829c1
AdeleM-prog <12043@holbertonstudents.com>
2026-02-23 17:03:16 +0000

    implementing Place class, to finalize

---
+++ part2/app/models/place.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+from app.models.user import User
+
+
+class Place(BaseModel):
+    def __init__(self, title, description, price, latitude, longitude, owner):
+        super().__init__()
+        if isinstance(title, str):
+            title = title.strip()
+        else:
+            raise TypeError("Title must be a string")
+        if title=="":
+            raise ValueError("Title cannot be empty")
+        if len(title) > 100:
+            raise ValueError("Title must be <= 100 characters")
+        self.title = title
+
+        if isinstance(description, str) or description is None:
+            self.description = description
+        else:
+            raise TypeError("Description must be a string or None")
+
+        if isinstance(price, (int, float)):
+            if isinstance(price, bool):
+                raise TypeError("Price must be a number")
+            if price <= 0:
+                raise ValueError("Price must be positive")
+            self.price = price
+        else:
+            raise TypeError("Price must be a number")
+
+        if isinstance(latitude, (int, float)):
+            if isinstance(latitude, bool):
+                raise TypeError("Latitude must be a number")
+            if -90.0 <= latitude <= 90:
+                self.latitude = latitude
+            else:
+                raise ValueError("Latitude must be between -90 and 90")
+        else:
+            raise TypeError("Latitude must be a number")
+
+        if isinstance(longitude, (int, float)):
+            if isinstance(longitude, bool):
+                raise TypeError("Longitude must be a number")
+            if -180.0 <= longitude <= 180:
+                self.longitude = longitude
+            else:
+                raise ValueError("Longitude must be between -180 and 180")
+        else:
+            raise TypeError("Longitude must be a number")
+
+        if isinstance(owner, User):
+            self.owner = owner
+        else:
+            raise TypeError("Owner must be an instance of User class")
+
+        self.reviews = []  # List to store related reviews
+        self.amenities = []  # List to store related amenities
+    def add_review(self, review):
+        """Add a review to the place."""
+        self.reviews.append(review)
+
+    def add_amenity(self, amenity):
+        """Add an amenity to the place."""
+        self.amenities.append(amenity)
+    
+    def update(self, data):
