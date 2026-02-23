452ae39605dd1ab50e41406f97ab8f01fdc86a02
FelixBesancon <f.besancon@hotmail.fr>
2026-02-23 11:13:40 +0000

    Initialize Part 2 project structure: - Created base.py file (structure placeholder) for implementation of Base class (inheritence of id, update_at and created_at attributes) - Added module, class and method docstrings, to assure compliance of the project structure with PEP8

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,13 +1,36 @@
 #!/usr/bin/python3
+"""
+This module provides the create_app function
+It defines the Flask application factory used to initialize the HBnB API
+and configure Flask-RESTx.
+"""
+
 
 from flask import Flask
 from flask_restx import Api
 
+
 def create_app():
+    """
+    Application factory function.
+
+    Creates and configures the Flask application instance,
+    initializes the Flask-RESTx API, and prepares the
+    structure for registering namespaces and endpoints.
+
+    Returns:
+        Flask: The configured Flask application instance.
+        """
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    api = Api(
+        app, version='1.0',
+        title='HBnB API',
+        description='HBnB Application API',
+        doc='/api/v1/'
+        )
 
     # Placeholder for API namespaces (endpoints will be added later)
-    # Additional namespaces for places, reviews, and amenities will be added later
+    # Additional namespaces for places, reviews, and amenities
+    # will be added later.
 
     return app
--- part2/run.py
+++ part2/run.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+Application entry point.
+
+This module creates the Flask application instance using the
+application factory and runs the development server.
+
+It serves as the main execution point for starting the HBnB API
+and runs the Flask development server.
+"""
+
 
 from app import create_app
 
