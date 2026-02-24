faa6ba79da6f76e2b5a02a1dab8cd65e216ec051
FelixBesancon <f.besancon@hotmail.fr>
2026-02-24 13:24:25 +0000

    Add mandatory endpoints of user class

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -8,6 +8,7 @@ The facade acts as the single entry point between the Presentation layer
 
 
 from app.persistence.repository import InMemoryRepository
+from ..models.user import User
 
 
 class HBnBFacade:
@@ -27,37 +28,22 @@ class HBnBFacade:
         is managed by a dedicated repository instance.
         """
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
         """
         Create a new user.
 
-        Args:
-            user_data (dict): A dictionary containing user information.
-
-        Note:
-            Business logic implementation will be added in later tasks.
         """
-        # Logic will be implemented in later tasks
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
+    def get_user(self, user_id):
         """
-        Retrieve a place by its unique identifier.
-
-        Args:
-            place_id (str): The unique identifier of the place.
-
-        Returns:
-            The place object if found, otherwise None.
-
-        Note:
-            Business logic implementation will be added in later tasks.
         """
-        # Logic will be implemented in later tasks
-        pass
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        """
+        """
+        return self.user_repo.get_by_attribute('email', email)
