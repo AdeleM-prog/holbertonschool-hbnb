4db56bb81f85392bbc87d74fcfc8153e1336abea
AdeleM-prog <12043@holbertonstudents.com>
2026-02-24 10:46:54 +0000

    suppr Amenities' description to fit the requirements

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -4,7 +4,7 @@ from .base import BaseModel
 
 
 class Amenity(BaseModel):
-    def __init__(self, name, description=None):
+    def __init__(self, name):
         super().__init__()
 
         if isinstance(name, str):
@@ -17,16 +17,3 @@ class Amenity(BaseModel):
                 raise ValueError("Name must be 50 characters maximum")
         else:
             raise TypeError("Name must be a string")
-
-        if description is None:
-            self.description = description
-        else:
-            if isinstance(description, str):
-                if len(description) <= 150:
-                    self.description = description
-                else:
-                    raise ValueError(
-                        "Description must be maximum 150 characters"
-                        )
-            else:
-                raise TypeError("Description must be a string")
